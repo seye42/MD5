c9a5bf27a8b6e93156e7d60839f6b10a75c6e340
seye42 <24215757+seye42@users.noreply.github.com>
2018-06-09 13:21:37 -0400

    Cleaned up comments

--- genTArray.m
+++ genTArray.m
@@ -1,14 +1,14 @@
-% build MD5 table of 64 T values
-% based on section 4.3.2 of Oppliger's Contemporary Cryptography, Second Edition
+% Build MD5 table of T values as a C constant global array (64 elements, 256 B)
+% Values are based on "Oppliger, Rolf. Contemporary cryptography. Artech House, 2011.", section
+% 4.3.2.
 
 %% generate the table
 i = 1:64;
 T = floor(2^32 * abs(sin(i)));
 
-%% display it in a pasteable form as a static C array
+%% display it C syntax
 printf("const uint32_t T[64] = {0x%08X, 0x%08X, 0x%08X, 0x%08X,\n", T(1:4));
 for idx = 1:14
-  printf("                        0x%08X, 0x%08X, 0x%08X, 0x%08X,\n",
-         T(idx * 4 + 1:(idx + 1) * 4));
+  printf("                        0x%08X, 0x%08X, 0x%08X, 0x%08X,\n", T(idx * 4 + 1:(idx + 1) * 4));
 end
 printf("                        0x%08X, 0x%08X, 0x%08X, 0x%08X};\n", T(61:64));
